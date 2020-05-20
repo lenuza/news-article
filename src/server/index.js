fed11b7b3a5609f27d5fd1194421525976b25693
Elena Nedkova <lenuza@gmail.com>
2020-05-20 18:56:19 +0300

    added aylien api and env

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 const path  = require('path')
 const express = require('express')
+const aylien = require("aylien_textapi")
+const dotenv = require('dotenv');
 
+dotenv.config()
 
 const app = express()
 
@@ -15,3 +18,9 @@ const port = process.env.port || 8000;
 const server = app.listen(port, () => {
     console.log(`Hello, listening on port ${port}`);
 });
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+})
