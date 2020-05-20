a8c3dfd6a51862386ede822fcb444f3971c4894f
Elena Nedkova <lenuza@gmail.com>
2020-05-20 15:38:33 +0300

    added styel, sass, css loaders and node-sass

--- webpack.dev.js
+++ webpack.dev.js
@@ -18,6 +18,10 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
