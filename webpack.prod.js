839cb8729f1018bee1ff61acf6fee49af799ca21
Elena Nedkova <lenuza@gmail.com>
2020-05-20 17:37:24 +0300

    added workbox plugin and script in index.html

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -37,5 +38,6 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css'
         }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
