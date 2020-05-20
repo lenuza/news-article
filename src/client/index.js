5cad860ed5139e7abf47393da2e2e7a6784a8c6f
Elena Nedkova <lenuza@gmail.com>
2020-05-20 20:11:37 +0300

    added jest

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,18 @@
 import './styles.scss'
+// var aylien = require("aylien_textapi")
+
+// var textapi = new aylien({
+//     application_id: process.env.API_ID,
+//     application_key: process.env.API_KEY
+// })
+
+var news = document.getElementById('news').innerHTML
+console.log(news)
+
+textapi.sentiment({
+    'text': news
+}, function(error, response) {
+    if (error === null) {
+        console.log(response);
+        }
+    });
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const path  = require('path')
 const express = require('express')
-const aylien = require("aylien_textapi")
+var aylien = require("aylien_textapi")
 const dotenv = require('dotenv');
 
 dotenv.config()
@@ -24,3 +24,14 @@ var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 })
+
+// var news = document.getElementById('news').innerHTML;
+// console.log(news)
+
+textapi.sentiment({
+    'text': ' Wearing masks is being made compulsory in Spain both indoors and out in public if social distancing is not possible.Only children under six and people with health issues are exempt from the law, which comes into force on Thursday. Many European countries have now made wearing masks a requirement on public transport but the Spanish decree goes further. Spain has seen one of the worst Covid-19 outbreaks in Europe but is now easing the lockdown gradually. It already requires the wearing of masks on public transport and is now strengthening the rules across the population. Spain has reported almost 28,000 deaths and 232,000 infections since March but the rate of infection has declined. Spain had imposed some of the toughest measures on the continent, including keeping children indoors for six weeks. Prime Minister Pedro Sánchez addressed parliament on Wednesday ahead of a vote on extending the state of alert for two more weeks.'
+}, function(error, response) {
+    if (error === null) {
+        console.log(response);
+        }
+    });
